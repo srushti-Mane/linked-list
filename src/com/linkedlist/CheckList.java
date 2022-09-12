33a40f04902d3fe67cd9687f71a9f258dce2afe7
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-09-12 14:46:27 +0530

    modified:logic for append

---
+++ src/com/linkedlist/CheckList.java
@@ -0,0 +1,12 @@
+package com.linkedlist;
+
+public class CheckList {
+    public static void main(String[] args) {
+        System.out.println("welcome to linked list program");
+        MyLinkedList list= new MyLinkedList();
+        list.append(56);
+        list.append(30);
+        list.append(70);
+        list.show();
+    }
+}
---
+++ src/com/linkedlist/MyLinkedList.java
@@ -0,0 +1,36 @@
+package com.linkedlist;
+
+public class MyLinkedList {
+    Node head;
+    Node tail;
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    } public void show(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+}
+
